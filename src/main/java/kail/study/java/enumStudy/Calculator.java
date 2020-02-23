13369d5e01e27e71338700a5378c71118cd2bd6c
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-23 20:09:34 +0900

    Add enum exmaple

---
+++ src/main/java/kail/study/java/enumStudy/Calculator.java
@@ -0,0 +1,36 @@
+package kail.study.java.enumStudy;
+
+public interface Calculator {
+	int calculate(int a, int b);
+}
+
+class Plus implements Calculator{
+	@Override
+	public int calculate(int a, int b) {
+		return a + b;
+	}
+}
+
+class Minus implements Calculator{
+	@Override
+	public int calculate(int a, int b) {
+		return a - b;
+	}
+}
+
+class Multiply implements Calculator{
+	@Override
+	public int calculate(int a, int b) {
+		return a * b;
+	}
+}
+
+class Divide implements Calculator{
+	@Override
+	public int calculate(int a, int b) {
+		if(b == 0)
+			throw new ArithmeticException();
+		return a / b;
+	}
+}
+
---
+++ src/main/java/kail/study/java/enumStudy/Rank.java
@@ -0,0 +1,35 @@
+package kail.study.java.enumStudy;
+
+import java.util.Arrays;
+
+public enum Rank {
+	THREE("셋", 3, 4_000),
+	FOUR("넷", 4, 10_000),
+	FIVE("다섯", 5, 30_000);
+
+	private final String name;
+	private final int match;
+	private final int money;
+	private int count;
+
+	Rank(String name, int match, int money) {
+		this.name = name;
+		this.match = match;
+		this.money = money;
+	}
+
+	public void plusCount() {
+		this.count++;
+	}
+
+	public void of(String anotherName) {
+		Arrays.stream(values())
+			.filter(name -> name.equals(anotherName))
+			.findFirst()
+			.orElseThrow(()-> new IllegalArgumentException());
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
