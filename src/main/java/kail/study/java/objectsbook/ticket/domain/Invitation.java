dd0fffcafb745feb79ad6d60fbfb4709cfc40a40
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-28 11:45:16 +0900

    feat : Add domains on ticket program

---
+++ src/main/java/kail/study/java/objectsbook/ticket/domain/Invitation.java
@@ -0,0 +1,7 @@
+package kail.study.java.objectsbook.ticket.domain;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+	private LocalDateTime when;
+}
---
+++ src/main/java/kail/study/java/objectsbook/ticket/domain/Ticket.java
@@ -0,0 +1,9 @@
+package kail.study.java.objectsbook.ticket.domain;
+
+public class Ticket {
+	private Long fee;
+
+	public Long getFee() {
+		return fee;
+	}
+}
